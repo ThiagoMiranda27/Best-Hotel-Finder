ef30918a70bb19252f2efcff135cba6626b6936b
Frederico Jacobi <fredvjacobi@gmail.com>
2020-06-16 22:06:17 -0300

    Update Telas

--- app/src/main/java/com/example/besthotelfinder/MainActivity.java
+++ app/src/main/java/com/example/besthotelfinder/MainActivity.java
@@ -7,21 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn_Entrar = (Button) findViewById(R.id.btn_Entrar);
-        btn_Entrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), TestActivity.class);
-                startActivity(intent);
-            }
-        });
+
+
+        Button btn_Entrar = (Button) findViewById(R.id.btn_Dados);
+
     }
-}
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.rb_admin:
+                Intent adm = new Intent(MainActivity.this, CadastroHotel.class);
+                startActivity(adm);
+                break;
+            case R.id.rb_usuario:
+                Intent usuario = new Intent(MainActivity.this, BuscaHotel.class);
+                startActivity(usuario);
+                break;
+
+        }
+}}
